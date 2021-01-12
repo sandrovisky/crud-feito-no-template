a814c9abd7effdd7d74e95ed807bd758694b99a4
sandrovisky <sndr95@hotmail.com>
2021-01-12 05:42:28 -0300

    sei la

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,21 @@
-const ufs = axios.create({
-    baseURL: 'https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome'
-});
+
+      var opt = window.document.getElementById('estados')
+     var apiUrl = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome';
+     var txt = 'masoq'
+
+     fetch(apiUrl).then(response => {
+       return response.json();
+     }).then(data => {
+        data.map(function(estado) {
+            console.log(estado.sigla);
+            
+        })
+       console.log(data);
+     }).catch(err => {
+       // Do something for an error here
+     });
+   
+
+   
+
+
