7e72c4895baace3f0444aef26422621941493614
sandrovisky <sndr95@hotmail.com>
2021-01-14 04:36:49 -0300

    desenvolvidos os combobox

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,21 +1,63 @@
 
-      var opt = window.document.getElementById('estados')
-     var apiUrl = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome';
-     var txt = 'masoq'
+//Preencher o combobox com os estados
+var apiUrl = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome';
+var estados = document.getElementById("estados");
+var opt = document.createElement("option")
 
-     fetch(apiUrl).then(response => {
-       return response.json();
-     }).then(data => {
-        data.map(function(estado) {
-            console.log(estado.sigla);
+fetch(apiUrl).then(response => {
+    return response.json();
+}).then(data => {
+    data.map(function(estado, x) {
+        var opt = document.createElement("option")
+        opt.value = (estado.sigla);
+        opt.text = (estado.nome);
+        estados.add(opt, estados.options[x]);            
+    })
+    console.log(data)
+}).catch(err => {
+
+});
+//------------------------------------------------------------------
+
+
+
+//Função que vai preencher o combobox com as cidades
+function selecionacidade(){
+    var estado = document.getElementById("estados").value
+    var apiUrl = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${estado}/distritos?orderBy=nome`;
+    var cidades = document.getElementById("cidades");
+    
+    var length = cidades.options.length;
+    for (i = length-1; i >= 0; i--) {
+        cidades.options[i] = null;
+    }
+
+    if (estado !== ''){
+        cidades.disabled = false
+    }else {
+        cidades.disabled = true        
+        var opt = document.createElement("option")
+        opt.value = '';
+        opt.text = 'Selecione o Estado primeiro';
+        cidades.add(opt, cidades.options); 
+    }
+    
+
+    fetch(apiUrl).then(response => {
+        return response.json();
+    }).then(data => {
+        data.map(function(cidade, x) {
+            var opt = document.createElement("option")
+            opt.value = (cidade.sigla);
+            opt.text = (cidade.nome);
             
+            cidades.add(opt, cidades.options[x]);            
         })
-       console.log(data);
-     }).catch(err => {
-       // Do something for an error here
-     });
-   
+        console.log(data)
+    }).catch(err => {
+    
+    });
 
-   
-
+}
+//---------------------------------------------------------------------------
 
